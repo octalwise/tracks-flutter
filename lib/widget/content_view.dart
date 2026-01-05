3ade5ead263fa24ee50c2cc4a0a1857985d794b0
Ayush Kumar <github@ayukmr.com>
2026-01-04 21:39:53 -0800

    Update at flat time

--- lib/widget/content_view.dart
+++ lib/widget/content_view.dart
@@ -75,7 +75,7 @@ class ContentViewState extends ConsumerState<ContentView> {
           return;
         }
 
-        if (now.hour > 3 || (now.hour == 3 && now.minute > 0)) {
+        if (now.hour >= 3) {
           fetch(init: true);
           lastUpdate = now;
         }
